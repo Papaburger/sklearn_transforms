724cd8830e159807e55478dc9c3eb63c69cb1d0d
Papaburger <33644834+Papaburger@users.noreply.github.com>
2020-09-02 02:17:24 -0300

    Update setup.py
    eu nome no setup

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
             This is a sample python package for encapsulating custom
             tranforms from scikit-learn into Watson Machine Learning
       ''',
-      url='https://github.com/vnderlev/sklearn_transforms/',
-      author='Vanderlei Munhoz',
-      author_email='vnderlev@protonmail.ch',
+      url='https://github.com/Papaburger/sklearn_transforms',
+      author='Ferdinando Menezes',
+      author_email='ferdinandolm@hotmail.com',
       license='BSD',
       packages=[
             'my_custom_sklearn_transforms'
