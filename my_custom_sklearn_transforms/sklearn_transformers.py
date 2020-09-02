e8c12594a87849f6c0f11f196ee16488c898f285
Papaburger <33644834+Papaburger@users.noreply.github.com>
2020-09-02 02:12:10 -0300

    Update sklearn_transformers.py
    mudanças

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,7 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import RobustScaler
+from sklearn import preprocessing
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -14,3 +17,25 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+si = SimpleImputer(
+    missing_values=np.nan,  # os valores faltantes são do tipo ``np.nan`` (padrão Pandas)
+    strategy='most_frequent',  # a estratégia escolhida é a alteração do valor faltante por uma constante
+    fill_value=0,  # a constante que será usada para preenchimento dos valores faltantes é um int64=0.
+    verbose=0,
+    copy=True
+)
+
+transformer = RobustScaler().fit(X)
+transformer
+
+transformer.transform(X)
+
+scaler = preprocessing.StandardScaler().fit(X)
+scaler
+
+scaler.mean_
+
+scaler.scale_
+
+scaler.transform(X)
